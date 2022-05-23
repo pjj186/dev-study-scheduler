6fe046134070c7268a2653e2c09e183f03fe9807
Jong jae Park <pjj186@naver.com>
2022-05-23 23:39:13 +0900

    SETUP

--- src/page/indexPage.tsx
+++ src/page/indexPage.tsx
@@ -1,20 +1,9 @@
 import React from "react";
-import Grid from "@material-ui/core/Grid";
-import { makeStyles } from "@material-ui/core";
-import Typography from "@material-ui/core/Typography";
-
-const useStyles = makeStyles((theme) => ({
-  root: {
-    height: "100vh",
-  },
-}));
 
 export const IndexPage: React.FC = () => {
-  const classes = useStyles();
-
   return (
-    <Grid container className={classes.root}>
-      <Typography variant="h1">메인 페이지입니다!</Typography>
-    </Grid>
+    <div className=" bg-orange-500 w-20 h-20">
+      <span className=" text-orange-100">Hello</span>
+    </div>
   );
 };
