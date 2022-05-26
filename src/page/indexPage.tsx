e63a5c8d4fe14c88ec0892169348628c6741883a
Jong jae Park <pjj186@naver.com>
2022-05-26 10:47:27 +0900

    SETUP

--- src/page/indexPage.tsx
+++ src/page/indexPage.tsx
@@ -2,8 +2,10 @@ import React from "react";
 
 export const IndexPage: React.FC = () => {
   return (
-    <div className=" bg-orange-500 w-20 h-20">
-      <span className=" text-orange-100">Hello</span>
+    <div className="flex">
+      <p className=" text-orange-500">Hello</p>
+      <p className=" text-orange-500">Hello</p>
+      <p className=" text-orange-500">Hello</p>
     </div>
   );
 };
